162ca873f61cbeecf18f8f9a1a7f1b98199dd259
Hildev <hilda.urd.karlsson@gmail.com>
2025-01-02 13:25:29 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'Hildas Kasm Registry',
     description: 'Hildas store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: 'https://www.flaticon.com/free-icon/ubuntu_888879',
     listUrl: 'https://hildev.github.io/kasm-registry/',
     contactUrl: 'https://github.com/Hildev/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
